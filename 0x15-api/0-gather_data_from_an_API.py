efcdaf4c4cdb49411fe510a62ac68db0b4209329
JesseGreat <oluwatobilobaokunogbe@gmail.com>
2023-04-25 09:06:53 -0700

    get users todo lists

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,20 +4,21 @@ returns information about his/her TODO list progress."""
 import requests
 import sys
 
-# parameter that takes in the employee id
-employee_id = sys.argv[1]
+if __name__ == '__main__':
+#   parameter that takes in the employee id
+    employee_id = sys.argv[1]
 
-# Get Employee information
-employee_info = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)).json()
+#   Get Employee information
+    employee_info = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)).json()
 
-# Get employer to_do list
-todo_lists = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id)).json()
+#   Get employer to_do list
+    todo_lists = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id)).json()
 
-# Get no of tasks completed that is TRUE
-no_of_completed_tasks = sum(no_of_tasks['completed'] for no_of_tasks in todo_lists)
+#   Get no of tasks completed that is TRUE
+    no_of_completed_tasks = sum(no_of_tasks['completed'] for no_of_tasks in todo_lists)
 
-# Print progress report
-print("Employee {} is done with tasks({}/{}):".format(employee_info['name'], no_of_completed_tasks, len(todo_lists)))
-for no_of_tasks in todo_lists:
-    if no_of_tasks['completed']:
-        print("\t {}".format(no_of_tasks['title']))
+#   Print progress report
+    print("Employee {} is done with tasks({}/{}):".format(employee_info['name'], no_of_completed_tasks, len(todo_lists)))
+    for no_of_tasks in todo_lists:
+        if no_of_tasks['completed']:
+             print("\t {}".format(no_of_tasks['title']))
