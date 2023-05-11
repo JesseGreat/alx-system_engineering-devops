1bc7b028d6770688959b4b30c571ac30f166df20
JesseGreat <oluwatobilobaokunogbe@gmail.com>
2023-05-11 03:57:32 -0700

    remove f strings

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,11 +13,20 @@ def number_of_subscribers(subreddit):
     Query the Reddit API and return the number of subscribers for a given
     subreddit.
     If the subreddit is invalid, return 0.
+
+    Args:
+        subreddit (str): The name of the subreddit to query.
+
+    Returns:
+        int: The number of subscribers for the subreddit, or 0 if invalid.
+
+    Raises:
+        None
     """
     # Set custom User-Agent header to avoid Too Many Requests error
     headers = {'User-Agent': 'JesseGreat/0.1'}
     # Send GET request to the subreddit API endpoint
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = requests.get(url, headers=headers)
     # If the request was successful, return the number of subscribers
     if response.status_code == 200:
